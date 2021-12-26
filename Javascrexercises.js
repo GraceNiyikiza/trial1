5ac596e73d4572333e1cc72698a4d15f8de8aee2
GraceNiyikiza <95916239+GraceNiyikiza@users.noreply.github.com>
2021-12-26 18:58:05 +0200

    new programs

---
+++ Javascrexercises.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<style type="text/css">
+		h1{font-size: 40px;}
+		p{font-size: 35px;}
+	</style>
+</head>
+<body><h1>Can I give chocolate as Christmas gift?</h1>
+	<h2></h2>
+	<p>The holiday gift retail boom
+
+Fast forward to the year 2018, and Christmas is bigger than ever. ... That's why we think chocolate – and other candy – can make a good holiday gift. It's something most people enjoy, or at least can share with friends and family members. There's something for everyone in a box of chocolates.</p>
+	<p>1. Ferrero Rocher. It is said that Ferrero Rocher is the best selling brand of chocolate on Earth, with millions of people all over the world buying this chocolate every year. The company has been a leader in the chocolate industry for over 200 years.</p>
+
+</body>
+</html>
---
+++ javaexercises.js
@@ -0,0 +1,18 @@
+document.write("<h2>Most delicious cakes</h2>");
+document.write('<h2>"First one is"</h2>');
+
+
+
+
+var x=5;
+               document.write(x+=10)
+               document.write("<br>")
+               var t=5;
+               t+=2
+               document.write(t+)
+               document.write("<br>")
+               t--;
+               document.write(t--)
+               document.write("<br>")
+               t-=3;
+               document.write(t-)
